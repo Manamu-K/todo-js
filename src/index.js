1c4e79eff790065c55e233625519a6a251b4ae79
Manamu.K <64361362+Manamu-K@users.noreply.github.com>
2021-12-29 16:11:42 +0900

    値の取得と初期化

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-
-  const div = document.createElement("div");
-  div.className = "list-row";
-
-  const li = document.createElement("li");
-  li.innerText = inputText;
-
-  const completeButton = document.createElement("button");
-  completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget); 
-  });
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除";
-  deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
-  });
-
-  div.appendChild(li);
-  div.appendChild(completeButton);
-  div.appendChild(deleteButton);
-  document.getElementById("incomplete-list").appendChild(div);
+  alert(inputText);
 };
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (Target) => 
-  document.getElementById("incomplete-list").removeChild(deleteTarget); 
-}
 
 document
   .getElementById("add-button")
