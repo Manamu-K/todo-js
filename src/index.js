e24af5c6f72956967c77ff8938ff792ab27f21cb
Manamu.K <64361362+Manamu-K@users.noreply.github.com>
2021-12-30 18:35:05 +0900

    削除ボタン

--- src/index.js
+++ src/index.js
@@ -60,23 +60,30 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert();
+    // 押された完了ボタンの親タグ(div)をTODOから削除
+    deleteFromIncompleteList(completeButton.parentNode);
   });
+
   // button(削除)生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert();
+    // 押された削除ボタンの親タグ(div)をTODOから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-  console.log(div);
 
-  //  TODOに追加
+  // TODOに追加
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+// TODOから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
